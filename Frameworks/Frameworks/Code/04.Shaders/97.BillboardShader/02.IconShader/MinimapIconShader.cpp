d6452408355b6bf612b25169a4c08731c1ebae9c
YongsunLee <katarite715@naver.com>
2018-05-11 01:50:21 +0900

    UPload
    UPload

---
+++ Frameworks/Frameworks/Code/04.Shaders/97.BillboardShader/02.IconShader/MinimapIconShader.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "MinimapIconShader.h"
+
+CMinimapIconShader::CMinimapIconShader(CCreateMgr *pCreateMgr)
+	: CShader(pCreateMgr)
+{
+
+}
+
+CMinimapIconShader::~CMinimapIconShader()
+{
+}
