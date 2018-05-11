f45a54303aab501b5d31af746dbd4faac06120b0
YongsunLee <katarite715@naver.com>
2018-05-11 17:49:05 +0900

    Minimap Icon 작업진행중
    Minimap Icon 작업진행중

--- Frameworks/Frameworks/Code/04.Shaders/97.BillboardShader/02.IconShader/MinimapIconShader.cpp
+++ Frameworks/Frameworks/Code/04.Shaders/97.BillboardShader/02.IconShader/MinimapIconShader.cpp
@@ -1,5 +1,9 @@
 #include "stdafx.h"
 #include "MinimapIconShader.h"
+#include "05.Objects/95.Billboard/Billboard.h"
+#include "02.Framework/01.CreateMgr/CreateMgr.h"
+#include "05.Objects/99.Material/Material.h"
+#include "05.Objects/08.Player/Player.h"
 
 CMinimapIconShader::CMinimapIconShader(CCreateMgr *pCreateMgr)
 	: CShader(pCreateMgr)
@@ -63,10 +67,20 @@ void CMinimapIconShader::AnimateObjects(float timeElapsed)
 
 void CMinimapIconShader::Render(CCamera * pCamera)
 {
+	CShader::Render(pCamera);
+#if USE_BATCH_MATERIAL
+	if (m_ppMaterials) m_ppMaterials[0]->UpdateShaderVariables();
+#endif
+
+	for (int j = 0; j < m_nObjects; j++)
+	{
+		if (m_ppObjects[j]) m_ppObjects[j]->Render(pCamera);
+	}
 }
 
 void CMinimapIconShader::GetCamera(CCamera * pCamera)
 {
+	m_pCamera = pCamera;
 }
 
 bool CMinimapIconShader::OnProcessKeyInput(UCHAR * pKeyBuffer)
@@ -81,40 +95,170 @@ bool CMinimapIconShader::OnProcessMouseInput(WPARAM pKeyBuffer)
 
 D3D12_INPUT_LAYOUT_DESC CMinimapIconShader::CreateInputLayout()
 {
-	return D3D12_INPUT_LAYOUT_DESC();
+	UINT nInputElementDescs = 2;
+	D3D12_INPUT_ELEMENT_DESC *pd3dInputElementDescs = new D3D12_INPUT_ELEMENT_DESC[nInputElementDescs];
+
+	pd3dInputElementDescs[0] = {
+		"POSITION",
+		0,
+		DXGI_FORMAT_R32G32B32_FLOAT,
+		0,
+		0,
+		D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,
+		0 };
+	pd3dInputElementDescs[1] = {
+		"TEXCOORD",
+		0,
+		DXGI_FORMAT_R32G32_FLOAT,
+		0,
+		12,
+		D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,
+		0 };
+
+	D3D12_INPUT_LAYOUT_DESC d3dInputLayoutDesc;
+	d3dInputLayoutDesc.pInputElementDescs = pd3dInputElementDescs;
+	d3dInputLayoutDesc.NumElements = nInputElementDescs;
+
+	return(d3dInputLayoutDesc);
 }
 
 D3D12_BLEND_DESC CMinimapIconShader::CreateBlendState()
 {
-	return D3D12_BLEND_DESC();
+	D3D12_BLEND_DESC blendDesc;
+	::ZeroMemory(&blendDesc, sizeof(D3D12_BLEND_DESC));
+
+	blendDesc.AlphaToCoverageEnable = TRUE;
+	blendDesc.IndependentBlendEnable = FALSE;
+	blendDesc.RenderTarget[0].BlendEnable = FALSE;
+	blendDesc.RenderTarget[0].LogicOpEnable = FALSE;
+	blendDesc.RenderTarget[0].SrcBlend = D3D12_BLEND_ONE;
+	blendDesc.RenderTarget[0].DestBlend = D3D12_BLEND_ZERO;
+	blendDesc.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
+	blendDesc.RenderTarget[0].SrcBlendAlpha = D3D12_BLEND_ONE;
+	blendDesc.RenderTarget[0].DestBlendAlpha = D3D12_BLEND_ZERO;
+	blendDesc.RenderTarget[0].BlendOpAlpha = D3D12_BLEND_OP_ADD;
+	blendDesc.RenderTarget[0].LogicOp = D3D12_LOGIC_OP_NOOP;
+	blendDesc.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
+
+	return(blendDesc);
 }
 
 D3D12_SHADER_BYTECODE CMinimapIconShader::CreateVertexShader(ID3DBlob ** ppShaderBlob)
 {
-	return D3D12_SHADER_BYTECODE();
+#if USE_INSTANCING
+	return(CShader::CompileShaderFromFile(
+		L"./code/04.Shaders/99.GraphicsShader/Shaders.hlsl",
+		"VSTxtInstancing",
+		"vs_5_1",
+		ppShaderBlob));
+#else
+	return(CShader::CompileShaderFromFile(
+		L"./code/04.Shaders/99.GraphicsShader/Shaders.hlsl",
+		"VSTextured",
+		"vs_5_1",
+		ppShaderBlob));
+#endif
 }
 
 D3D12_SHADER_BYTECODE CMinimapIconShader::CreatePixelShader(ID3DBlob ** ppShaderBlob)
 {
-	return D3D12_SHADER_BYTECODE();
+	return(CShader::CompileShaderFromFile(
+		L"./code/04.Shaders/99.GraphicsShader/Shaders.hlsl",
+		"PSTextured",
+		"ps_5_1",
+		ppShaderBlob));
 }
 
 void CMinimapIconShader::CreateShader(CCreateMgr * pCreateMgr)
 {
+	m_nPipelineStates = 1;
+	m_ppPipelineStates = new ID3D12PipelineState*[m_nPipelineStates];
+
+	m_nHeaps = 1;
+	CreateDescriptorHeaps();
+
+	CShader::CreateShader(pCreateMgr);
 }
 
 void CMinimapIconShader::CreateShaderVariables(CCreateMgr * pCreateMgr, int nBuffers)
 {
+	HRESULT hResult;
+
+#if USE_INSTANCING
+	m_pInstanceBuffer = pCreateMgr->CreateBufferResource(
+		NULL,
+		sizeof(CB_GAMEOBJECT_INFO) * nBuffers,
+		D3D12_HEAP_TYPE_UPLOAD,
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		NULL);
+
+	hResult = m_pInstanceBuffer->Map(0, NULL, (void **)&m_pMappedObjects);
+	assert(SUCCEEDED(hResult) && "m_pInstanceBuffer->Map Failed");
+#else
+	UINT elementBytes = ((sizeof(CB_GAMEOBJECT_INFO) + 255) & ~255);
+
+	m_pConstBuffer = pCreateMgr->CreateBufferResource(
+		NULL,
+		elementBytes * nBuffers,
+		D3D12_HEAP_TYPE_UPLOAD,
+		D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER,
+		NULL);
+
+	hResult = m_pConstBuffer->Map(0, NULL, (void **)&m_pMappedObjects);
+	assert(SUCCEEDED(hResult) && "m_pConstBuffer->Map Failed");
+#endif
 }
 
 void CMinimapIconShader::BuildObjects(CCreateMgr * pCreateMgr, void * pContext)
 {
-}
+	m_pCamera = (CCamera*)pContext;
 
-void CMinimapIconShader::ReleaseShaderVariables()
-{
+	m_nObjects = m_nPlayer;
+	m_ppObjects = new CBaseObject*[m_nObjects];
+
+	UINT ncbElementBytes = ((sizeof(CB_GAMEOBJECT_INFO) + 255) & ~255);
+
+	CreateCbvAndSrvDescriptorHeaps(pCreateMgr, m_nObjects, 1);
+	CreateShaderVariables(pCreateMgr, m_nObjects);
+	CreateConstantBufferViews(pCreateMgr, m_nObjects, m_pConstBuffer, ncbElementBytes);
+
+	UINT incrementSize{ pCreateMgr->GetCbvSrvDescriptorIncrementSize() };
+	CMinimapIconObjects *pGaugeObject = NULL;
+
+	for (int i = 0; i < m_nObjects; ++i) {
+		pGaugeObject = new CMinimapIconObjects(pCreateMgr);
+		pGaugeObject->SetMaterial(Materials::CreateRedMaterial(pCreateMgr, &m_psrvCPUDescriptorStartHandle[0], &m_psrvGPUDescriptorStartHandle[0]));
+		pGaugeObject->SetCamera(m_pCamera);
+
+		pGaugeObject->SetDistance(FRAME_BUFFER_WIDTH / 142);	// distance 9
+
+		pGaugeObject->SetObject(m_pPlayer[i]);
+		
+		pGaugeObject->SetCbvGPUDescriptorHandlePtr(m_pcbvGPUDescriptorStartHandle[0].ptr + (incrementSize * i));
+		
+		m_ppObjects[i] = pGaugeObject;
+	}
 }
 
 void CMinimapIconShader::ReleaseObjects()
 {
+	if (m_ppObjects)
+	{
+		for (int j = 0; j < m_nObjects; j++)
+		{
+			delete m_ppObjects[j];
+		}
+		Safe_Delete_Array(m_ppObjects);
+	}
+
+#if USE_BATCH_MATERIAL
+	if (m_ppMaterials)
+	{
+		for (int i = 0; i < m_nMaterials; ++i)
+		{
+			if (m_ppMaterials[i]) delete m_ppMaterials[i];
+		}
+		Safe_Delete(m_ppMaterials);
+	}
+#endif
 }
