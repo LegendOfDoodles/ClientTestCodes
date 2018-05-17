d708272a84692c4ca1c9ceede87763ca0e3756a9
NadanKim <paintime@naver.com>
2018-05-18 00:15:27 +0900

    랜더링 처리 시 매터리얼 업데이트 한번씩만 되도록 Shader Render 방식 변경 사용하지 않는 쉐이더(Arrow Shader) 제거

--- Frameworks/Frameworks/Code/03.Scenes/00.BaseScene/Scene.cpp
+++ Frameworks/Frameworks/Code/03.Scenes/00.BaseScene/Scene.cpp
@@ -8,13 +8,12 @@
 #include "04.Shaders/04.AniShader/AniShader.h"
 #include "04.Shaders/05.PlayerShader/PlayerShader.h"
 #include "04.Shaders/06.NexusTowerShader/NexusTowerShader.h"
-#include "04.Shaders/97.BillboardShader/00.UIShader/UIShader.h"
-#include "04.Shaders/97.BillboardShader/01.GaugeShader/00.PlayerGaugeShader/PlayerGaugeShader.h"
-#include "04.Shaders/97.BillboardShader/01.GaugeShader/01.MinionGaugeShader/MinionGaugeShader.h"
-#include "04.Shaders/97.BillboardShader/01.GaugeShader/02.NexusAndTowerGaugeShader/NexusAndTowerGaugeShader.h"
-#include "04.Shaders/97.BillboardShader/02.IconShader/00.MinimapIconShader/MinimapIconShader.h"
-#include "04.Shaders/97.BillboardShader/02.IconShader/01.BuildingMinimapIconShader/BuildingMinimapIconShader.h"
-#include "04.Shaders/98.ArrowShader/ArrowShader.h"
+#include "04.Shaders/98.BillboardShader/00.UIShader/UIShader.h"
+#include "04.Shaders/98.BillboardShader/01.GaugeShader/00.PlayerGaugeShader/PlayerGaugeShader.h"
+#include "04.Shaders/98.BillboardShader/01.GaugeShader/01.MinionGaugeShader/MinionGaugeShader.h"
+#include "04.Shaders/98.BillboardShader/01.GaugeShader/02.NexusAndTowerGaugeShader/NexusAndTowerGaugeShader.h"
+#include "04.Shaders/98.BillboardShader/02.IconShader/00.MinimapIconShader/MinimapIconShader.h"
+#include "04.Shaders/98.BillboardShader/02.IconShader/01.BuildingMinimapIconShader/BuildingMinimapIconShader.h"
 #include "05.Objects/01.Camera/01.AOSCamera/AOSCamera.h"
 #include "00.Global/01.Utility/04.WayFinder/WayFinder.h"
 #include "00.Global/01.Utility/05.CollisionManager/CollisionManager.h"
@@ -25,7 +24,7 @@
 /// 목적: 기본 씬, 인터페이스 용
 /// 최종 수정자:  김나단
 /// 수정자 목록:  김나단
-/// 최종 수정 날짜: 2018-05-16
+/// 최종 수정 날짜: 2018-05-18
 /// </summary>
 
 ////////////////////////////////////////////////////////////////////////
@@ -157,9 +156,9 @@ void CScene::UpdateCamera()
 
 		m_pCamera->Initialize(m_pCreateMgr);
 
-		static_cast<CUIObjectShader*>(m_ppShaders[7])->GetCamera(m_pCamera);
-		static_cast<CPlayerHPGaugeShader*>(m_ppShaders[8])->GetCamera(m_pCamera);
-		static_cast<CMinionHPGaugeShader*>(m_ppShaders[9])->GetCamera(m_pCamera);
+		static_cast<CUIObjectShader*>(m_ppShaders[6])->GetCamera(m_pCamera);
+		static_cast<CPlayerHPGaugeShader*>(m_ppShaders[7])->GetCamera(m_pCamera);
+		static_cast<CMinionHPGaugeShader*>(m_ppShaders[8])->GetCamera(m_pCamera);
 
 
 		m_bCamChanged = false;
@@ -253,23 +252,22 @@ void CScene::BuildObjects(CCreateMgr *pCreateMgr)
 
 	m_pCamera->Initialize(pCreateMgr);
 
-	m_nShaders = 12;
+	m_nShaders = 11;
 	m_ppShaders = new CShader*[m_nShaders];
 	m_ppShaders[0] = new CSkyBoxShader(pCreateMgr);
 	CTerrainShader* pTerrainShader = new CTerrainShader(pCreateMgr);
 	m_ppShaders[1] = pTerrainShader;
 	m_ppShaders[2] = new CAniShader(pCreateMgr, m_pNetwork);
-	m_ppShaders[3] = new CArrowShader(pCreateMgr);
-	m_ppShaders[4] = new CStaticObjectShader(pCreateMgr);
-	m_ppShaders[5] = new CPlayerShader(pCreateMgr, m_pNetwork);
-	m_ppShaders[6] = new CNexusTowerShader(pCreateMgr);
+	m_ppShaders[3] = new CStaticObjectShader(pCreateMgr);
+	m_ppShaders[4] = new CPlayerShader(pCreateMgr, m_pNetwork);
+	m_ppShaders[5] = new CNexusTowerShader(pCreateMgr);
 	
 	//UI Shader
-	m_ppShaders[7] = new CUIObjectShader(pCreateMgr);
-	m_ppShaders[8] = new CPlayerHPGaugeShader(pCreateMgr);
-	m_ppShaders[9] = new CMinionHPGaugeShader(pCreateMgr);
-	m_ppShaders[10] = new CMinimapIconShader(pCreateMgr);
-	m_ppShaders[11] = new CBuildingMinimapIconShader(pCreateMgr);
+	m_ppShaders[6] = new CUIObjectShader(pCreateMgr);
+	m_ppShaders[7] = new CPlayerHPGaugeShader(pCreateMgr);
+	m_ppShaders[8] = new CMinionHPGaugeShader(pCreateMgr);
+	m_ppShaders[9] = new CMinimapIconShader(pCreateMgr);
+	m_ppShaders[10] = new CBuildingMinimapIconShader(pCreateMgr);
 
 	// Object Shaders Initialize
 	for (int i = 0; i < 2; ++i)
@@ -283,22 +281,22 @@ void CScene::BuildObjects(CCreateMgr *pCreateMgr)
 	}
 	
 	// UI Shaders Initialize
-	((CPlayerHPGaugeShader*)m_ppShaders[8])->SetPlayerCnt(((CPlayerShader *)m_ppShaders[5])->GetObjectCount());
-	((CPlayerHPGaugeShader*)m_ppShaders[8])->SetPlayer(((CPlayerShader *)m_ppShaders[5])->GetCollisionObjects());
-	((CPlayerHPGaugeShader*)m_ppShaders[8])->SetNexusAndTowerCnt(((CNexusTowerShader *)m_ppShaders[6])->GetObjectCount());
-	((CPlayerHPGaugeShader*)m_ppShaders[8])->SetNexusAndTower(((CNexusTowerShader *)m_ppShaders[6])->GetCollisionObjects());
+	((CPlayerHPGaugeShader*)m_ppShaders[7])->SetPlayerCnt(((CPlayerShader *)m_ppShaders[4])->GetObjectCount());
+	((CPlayerHPGaugeShader*)m_ppShaders[7])->SetPlayer(((CPlayerShader *)m_ppShaders[4])->GetCollisionObjects());
+	((CPlayerHPGaugeShader*)m_ppShaders[7])->SetNexusAndTowerCnt(((CNexusTowerShader *)m_ppShaders[5])->GetObjectCount());
+	((CPlayerHPGaugeShader*)m_ppShaders[7])->SetNexusAndTower(((CNexusTowerShader *)m_ppShaders[5])->GetCollisionObjects());
 
-	((CMinimapIconShader*)m_ppShaders[10])->SetPlayerCnt(((CPlayerShader *)m_ppShaders[5])->GetObjectCount());
-	((CMinimapIconShader*)m_ppShaders[10])->SetPlayer(((CPlayerShader *)m_ppShaders[5])->GetCollisionObjects());
+	((CMinimapIconShader*)m_ppShaders[9])->SetPlayerCnt(((CPlayerShader *)m_ppShaders[4])->GetObjectCount());
+	((CMinimapIconShader*)m_ppShaders[9])->SetPlayer(((CPlayerShader *)m_ppShaders[4])->GetCollisionObjects());
 
-	((CBuildingMinimapIconShader*)m_ppShaders[11])->SetNexusAndTowerCnt(((CNexusTowerShader *)m_ppShaders[6])->GetObjectCount());
-	((CBuildingMinimapIconShader*)m_ppShaders[11])->SetNexusAndTower(((CNexusTowerShader *)m_ppShaders[6])->GetCollisionObjects());
+	((CBuildingMinimapIconShader*)m_ppShaders[10])->SetNexusAndTowerCnt(((CNexusTowerShader *)m_ppShaders[5])->GetObjectCount());
+	((CBuildingMinimapIconShader*)m_ppShaders[10])->SetNexusAndTower(((CNexusTowerShader *)m_ppShaders[5])->GetCollisionObjects());
 
+	m_ppShaders[6]->Initialize(pCreateMgr, m_pCamera);
 	m_ppShaders[7]->Initialize(pCreateMgr, m_pCamera);
 	m_ppShaders[8]->Initialize(pCreateMgr, m_pCamera);
 	m_ppShaders[9]->Initialize(pCreateMgr, m_pCamera);
 	m_ppShaders[10]->Initialize(pCreateMgr, m_pCamera);
-	m_ppShaders[11]->Initialize(pCreateMgr, m_pCamera);
 
 	// Managere Initialize
 	m_pWayFinder = new CWayFinder();
@@ -313,10 +311,10 @@ void CScene::BuildObjects(CCreateMgr *pCreateMgr)
 	pAniS->SetGaugeManger(m_pUIObjectsManager);
 	pAniS->SetFSMManager(m_pFSMMgr);
 
-	static_cast<CMinionHPGaugeShader*>(m_ppShaders[9])->SetUIObjectsManager(m_pUIObjectsManager);
-	static_cast<CMinimapIconShader*>(m_ppShaders[10])->SetUIObjectsManager(m_pUIObjectsManager);
+	static_cast<CMinionHPGaugeShader*>(m_ppShaders[8])->SetUIObjectsManager(m_pUIObjectsManager);
+	static_cast<CMinimapIconShader*>(m_ppShaders[9])->SetUIObjectsManager(m_pUIObjectsManager);
 
-	CPlayerShader* pPlayerS = (CPlayerShader *)m_ppShaders[5];
+	CPlayerShader* pPlayerS = (CPlayerShader *)m_ppShaders[4];
 	int nColliderObject = pPlayerS->GetObjectCount();
 	for (int i = 0; i < nColliderObject; ++i)
 	{
@@ -324,7 +322,7 @@ void CScene::BuildObjects(CCreateMgr *pCreateMgr)
 	}
 	pPlayerS->SetColManagerToObject(m_pCollisionManager);
 	
-	CNexusTowerShader* pNTS = (CNexusTowerShader *)m_ppShaders[6];
+	CNexusTowerShader* pNTS = (CNexusTowerShader *)m_ppShaders[5];
 	nColliderObject = pNTS->GetObjectCount();
 	for (int i = 0; i < nColliderObject; ++i)
 	{
@@ -410,7 +408,7 @@ void CScene::PickObjectPointedByCursor(WPARAM wParam, LPARAM lParam)
 			// Status 창 띄우기 수도 코드
 			// 현재 6번 쉐이더가 UI 이므로 상호작용하는 Object의 타입을 받아와서
 			// 그 해당 오브젝트에 대한 정보를 출력
-			m_ppShaders[7]->OnStatus(pIntersectedObject->GetType());
+			m_ppShaders[6]->OnStatus(pIntersectedObject->GetType());
 
 			//m_Network.StartRecv(m_pSelectedObject);
 		}
