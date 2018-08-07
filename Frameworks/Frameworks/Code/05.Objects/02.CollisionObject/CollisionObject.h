f3fd097c84485c4a1376b63db1ce114fe3f43438
YongsunLee <katarite715@naver.com>
2018-08-07 22:04:04 +0900

    EffectShader
    EffectShader

--- Frameworks/Frameworks/Code/05.Objects/02.CollisionObject/CollisionObject.h
+++ Frameworks/Frameworks/Code/05.Objects/02.CollisionObject/CollisionObject.h
@@ -54,6 +54,7 @@ public: // 공개 함수
 	virtual CommonInfo* GetCommonStatus()			{ return NULL; }
 	virtual StaticInfo* GetNexusAndTowerStatus()	{ return NULL; }
 	virtual void SetFlyingObjectsType(FlyingObjectType type) { type; }
+	virtual void SetEffectObjectsType(EffectObjectType type) { type; }
 	virtual void SetDirection(const XMFLOAT3& direction) { direction; }
 	virtual FlyingObjectType GetFlyingObjectsType() { return FlyingObjectType::Roider_Dumbel; }
 
--- Frameworks/Frameworks/Code/05.Objects/96.Billboard/06.EffectObject/EffectObject.cpp
+++ Frameworks/Frameworks/Code/05.Objects/96.Billboard/06.EffectObject/EffectObject.cpp
@@ -10,7 +10,7 @@ CEffectObject::CEffectObject(shared_ptr<CCreateMgr> pCreateMgr, int nMeshes)
 	m_ObjectType = ObjectType::EffectObject;
 
 	// HP게이지 Mesh
-	CTexturedRectMesh *pRectMesh = new CTexturedRectMesh(pCreateMgr, FRAME_BUFFER_WIDTH / 40.f, FRAME_BUFFER_HEIGHT / 22.5f, 0.f);
+	CTexturedRectMesh *pRectMesh = new CTexturedRectMesh(pCreateMgr, FRAME_BUFFER_WIDTH / 12.8f, FRAME_BUFFER_HEIGHT / 7.2f, 0.f);
 	SetMesh(0, pRectMesh);
 }
 
@@ -25,22 +25,45 @@ void CEffectObject::Animate(float timeElapsed)
 	if (!m_Activated) return;
 
 	// Billboard Update
-	XMFLOAT3 xmf3Up = m_pCamera->GetUpVector();
-	XMFLOAT3 xmf3Position(m_xmf4x4World._41, m_xmf4x4World._42, m_xmf4x4World._43);
-	XMFLOAT3 xmf3Look = Vector3::ScalarProduct(m_pCamera->GetLookVector(), -1);
-	XMFLOAT3 xmf3Right = Vector3::ScalarProduct(m_pCamera->GetRightVector(), -1);
-
-	m_xmf4x4World._11 = xmf3Right.x;	m_xmf4x4World._12 = xmf3Right.y;	m_xmf4x4World._13 = xmf3Right.z;
-	m_xmf4x4World._21 = xmf3Up.x;		m_xmf4x4World._22 = xmf3Up.y;		m_xmf4x4World._23 = xmf3Up.z;
-	m_xmf4x4World._31 = xmf3Look.x;		m_xmf4x4World._32 = xmf3Look.y;
-
+	//XMFLOAT3 xmf3Up = m_pCamera->GetUpVector();
+	//XMFLOAT3 xmf3Position(m_xmf4x4World._41, m_xmf4x4World._42, m_xmf4x4World._43);
+	//XMFLOAT3 xmf3Look = Vector3::ScalarProduct(m_pCamera->GetLookVector(), -1);
+	//XMFLOAT3 xmf3Right = Vector3::ScalarProduct(m_pCamera->GetRightVector(), -1);
+	//
+	//m_xmf4x4World._11 = xmf3Right.x;	m_xmf4x4World._12 = xmf3Right.y;	m_xmf4x4World._13 = xmf3Right.z;
+	//m_xmf4x4World._21 = xmf3Up.x;		m_xmf4x4World._22 = xmf3Up.y;		m_xmf4x4World._23 = xmf3Up.z;
+	//m_xmf4x4World._31 = xmf3Look.x;		m_xmf4x4World._32 = xmf3Look.y;
+	
 	// Effect Update
 	if (m_EffectObjectType == EffectObjectType::Player_SwordSkill_Q)
 	{
-		m_AnimaitonTime += timeElapsed;
+		m_AnimaitonTime += timeElapsed * 4.f;
 		if (m_AnimaitonTime > m_maxAnimaitonTime)
 		{
 			m_curState = StatesType::Remove;
+		}
+	}
+}
+
+void CEffectObject::Render(CCamera * pCamera, UINT istanceCnt)
+{
+	OnPrepareRender();
+	//if (!IsVisible(pCamera) || !m_pMasterObject->GetDetected()) return;
+
+	if (m_pMaterial)
+	{
+		m_pMaterial->Render(pCamera);
+		m_pMaterial->UpdateShaderVariables();
+	}
+
+	if (m_cbvGPUDescriptorHandle.ptr)
+		m_pCommandList->SetGraphicsRootDescriptorTable(10, m_cbvGPUDescriptorHandle);
+
+	if (m_ppMeshes)
+	{
+		for (int i = 0; i < m_nMeshes; i++)
+		{
+			if (m_ppMeshes[i]) m_ppMeshes[i]->Render(istanceCnt);
 		}
 	}
 }
@@ -49,6 +72,17 @@ void CEffectObject::SetDirection(const XMFLOAT3 & direction)
 {
 	m_direction = direction;
 	LookAt(Vector3::Add(GetPosition(), Vector3::ScalarProduct(direction, 10000, false)));
+}
+
+void CEffectObject::SetEffectObjectsType(EffectObjectType type)
+{
+	m_EffectObjectType = type;
+	m_curState = StatesType::Idle;
+
+	if (type == EffectObjectType::Player_SwordSkill_Q)
+	{
+		m_AnimaitonTime = 0.0f;
+	}
 }
 
 void CEffectObject::LookAt(XMFLOAT3 target)
@@ -74,9 +108,9 @@ void CEffectObject::LookAt(XMFLOAT3 target)
 
 	// 회전 방향 결정
 	if (check < 0.0f)
-		Rotate(0.0f, angle, 0.0f);
+		Rotate(-90.0f, angle + 180.f, 0.0f);
 	else if (check > 0.0f)
-		Rotate(0.0f, -angle, 0.0f);
+		Rotate(-90.0f, -angle - 180.f, 0.0f);
 }
 
 void CEffectObject::LookAt(XMFLOAT2 target)
